45b6cfe5243f9415b4b57af9b4fe1f8707393b6b
K-Akira928 <K.AkiraRei928@gmail.com>
2024-01-08 23:23:30 +0900

    引数を元にDateを返す関数を作成

---
+++ javascript/calendar.js
@@ -0,0 +1,16 @@
+const commandGetDate = () => {
+  if (process.argv[2] === '-m' && process.argv[3] >= 1 && process.argv[3] <= 12) {
+    const date = new Date();
+    date.setMonth(process.argv[3] - 1, 1);
+    return date;
+  }
+
+  if (process.argv[2] === '-m' && process.argv[3] > 1 || process.argv[3] < 12) {
+    throw new SyntaxError(`${process.argv[3]} is neither a month number (1..12) nor a name`);
+
+  } else {
+    const nowDate = new Date();
+    nowDate.setDate(1);
+    return nowDate;
+  }
+};
